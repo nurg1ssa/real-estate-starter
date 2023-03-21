bb684457b726def35d553d73497f0b30da8cd281
nurg1ssa <nurike2001@mail.ru>
2023-03-21 21:36:55 +0100

    footer is set

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer className='bg-black py-8 text-center text-white'>
+        <div className="container">
+           Copyright &copy; 2022. All rights reserved.
+        </div>
+    </footer>
+  );
+};
+
+export default Footer;
